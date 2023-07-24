ddffd285478a9808e2cf50faf139bff3ae2e2ef0
Diego Massarini <63283003+webdiego@users.noreply.github.com>
2023-07-24 13:25:09 +0200

    clean up and add framer motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Pacifico } from 'next/font/google'
 import Link from 'next/link'
 const pacifico = Pacifico({ weight: '400', subsets: ['latin'] })
-
+import { motion } from 'framer-motion'
 export default function Example() {
   return (
     <div className="bg-white">
@@ -41,48 +41,58 @@ export default function Example() {
             <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
               <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                 <div className="relative">
-                  <img
+                  <motion.img
+                    initial={{ y: '-10px', opacity: 0 }}
+                    animate={{ y: '0', opacity: 1 }}
+                    transition={{ duration: 1.5 }}
                     src="https://plus.unsplash.com/premium_photo-1676528716924-2ce3caaec811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2591&q=80"
                     alt=""
                     className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                   />
-                  <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                 </div>
               </div>
               <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                 <div className="relative">
-                  <img
+                  <motion.img
+                    initial={{ y: '-10px', opacity: 0 }}
+                    animate={{ y: '0', opacity: 1 }}
+                    transition={{ duration: 1.5, delay: 0.2 }}
                     src="https://images.unsplash.com/photo-1565855307023-a04f63ba6a4c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
                     alt=""
                     className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                   />
-                  <div className="pointer-events-none absolute inset-0 rounded-xl" />
                 </div>
                 <div className="relative">
-                  <img
+                  <motion.img
+                    initial={{ y: '-10px', opacity: 0 }}
+                    animate={{ y: '0', opacity: 1 }}
+                    transition={{ duration: 1.5, delay: 0.4 }}
                     src="https://images.unsplash.com/photo-1642114979822-c130e1db32d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=872&q=80"
                     alt=""
                     className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                   />
-                  <div className="pointer-events-none absolute inset-0 rounded-xl " />
                 </div>
               </div>
               <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                 <div className="relative">
-                  <img
+                  <motion.img
+                    initial={{ y: '-10px', opacity: 0 }}
+                    animate={{ y: '0', opacity: 1 }}
+                    transition={{ duration: 1.5, delay: 0.6 }}
                     src="https://images.unsplash.com/photo-1634469489748-3cb1c5fe4148?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
                     alt=""
                     className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                   />
-                  <div className="pointer-events-none absolute inset-0 rounded-xl " />
                 </div>
                 <div className="relative">
-                  <img
+                  <motion.img
+                    initial={{ y: '-10px', opacity: 0 }}
+                    animate={{ y: '0', opacity: 1 }}
+                    transition={{ duration: 1.5, delay: 0.8 }}
                     src="https://images.unsplash.com/photo-1571403712587-f99cedc8cf99?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80"
                     alt=""
                     className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                   />
-                  <div className="pointer-events-none absolute inset-0 rounded-xl " />
                 </div>
               </div>
             </div>
