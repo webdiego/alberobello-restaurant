fe96690817736d662750cc3314e3f6fb9e41b058
Diego Massarni <webdiegomassarini@gmail.com>
2025-07-27 16:17:59 +0200

    update gallery images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,34 +2,34 @@ import React from 'react'
 
 export default function Gallery() {
   return (
-    <div className="relative flex  flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div className="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
       <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="pointer-events-none relative flex gap-10 overflow-hidden">
         <div className="animate-marquee flex min-w-full shrink-0 items-center justify-around gap-10">
           <img
             className="img-gallery"
             src="https://static.gamberorosso.it/2021/04/orecchiette-con-le-cime-di-rapa.jpg"
-            alt=""
+            alt="Orecchiette con le cime di rapa"
           />
           <img
             className="img-gallery"
             src="https://plus.unsplash.com/premium_photo-1669261880961-ea68f9a2b7f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
-            alt=""
+            alt="Polpo alla griglia"
           />
           <img
             className="img-gallery"
-            src="https://blog.giallozafferano.it/sayummy/wp-content/uploads/2014/10/04-ottobre-2014-04-50-11-DSC_4138.jpg"
-            alt=""
+            src="https://images.unsplash.com/photo-1622880833523-7cf1c0bd4296?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Pizza"
           />
           <img
             className="img-gallery"
             src="https://images.unsplash.com/photo-1555072930-714bba1d24e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
-            alt=""
+            alt="Burrata"
           />
           <img
             className="img-gallery"
             src="https://images.unsplash.com/photo-1627626775846-122b778965ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
-            alt=""
+            alt="Pizza"
           />
         </div>
         <div
@@ -39,27 +39,27 @@ export default function Gallery() {
           <img
             className="img-gallery"
             src="https://static.gamberorosso.it/2021/04/orecchiette-con-le-cime-di-rapa.jpg"
-            alt=""
+            alt="Orecchiette con le cime di rapa"
           />
           <img
             className="img-gallery"
             src="https://plus.unsplash.com/premium_photo-1669261880961-ea68f9a2b7f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
-            alt=""
+            alt="Polpo alla griglia"
           />
           <img
             className="img-gallery"
             src="https://blog.giallozafferano.it/sayummy/wp-content/uploads/2014/10/04-ottobre-2014-04-50-11-DSC_4138.jpg"
-            alt=""
+            alt="Pizza"
           />
           <img
             className="img-gallery"
             src="https://images.unsplash.com/photo-1555072930-714bba1d24e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
-            alt=""
+            alt="Burrata"
           />
           <img
             className="img-gallery"
             src="https://images.unsplash.com/photo-1627626775846-122b778965ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
-            alt=""
+            alt="Pizza"
           />
         </div>
       </div>
--- src/components/WorkingHours.tsx
+++ src/components/WorkingHours.tsx
@@ -8,43 +8,43 @@ const workingHours = [
   },
   {
     day: 'Martedì',
-    hours: '12:00 - 14:30 / 19:00 - 22:30',
+    hours: '19:00 - 22:30',
   },
   {
-    day: 'Mercoledì',
+    day: 'Mercoledì - Domenica',
     hours: '12:00 - 14:30 / 19:00 - 22:30',
   },
-  { day: 'Giovedì', hours: '12:00 - 14:30 / 19:00 - 22:30' },
-  { day: 'Venerdì', hours: '12:00 - 14:30 / 19:00 - 22:30' },
-  { day: 'Sabato', hours: '12:00 - 14:30 / 19:00 - 22:30' },
-  { day: 'Domenica', hours: '12:00 - 14:30 / 19:00 - 22:30 ' },
 ]
 export default function WorkingHours() {
   return (
-    <div className="bg-white mt-28 px-6">
+    <div className="bg-white py-16 px-6">
       <div
         id="orari"
-        className="mx-auto max-w-2xl lg:max-w-7xl px-6 lg:px-8 py-16 grid grid-cols-1 lg:grid-cols-2"
+        className="mx-auto max-w-2xl lg:max-w-7xl px-6 lg:px-10 py-16 grid grid-cols-1 lg:grid-cols-2"
       >
-        <div className="max-w-sm">
+        <div className="">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Orari di apertura
           </h2>
           <div className="leading-7 text-gray-600 mt-10">
             {workingHours.map((item, index) => (
-              <div key={index} className="flex justify-between py-1">
+              <div
+                key={index}
+                className="flex justify-between py-1 text-sm md:text-base text"
+              >
                 <p>{item.day}</p>
-                <p>{item.hours}</p>
+                <p className="text-right">{item.hours}</p>
               </div>
             ))}
           </div>
         </div>
-        <div className="flex items-center mt-5 md:mt-0">
+        <div className="flex items-center mt-5 md:mt-10 lg:mx-10 ">
           <Image
             src="/contact.avif"
             alt="Orari di apertura"
-            width={500}
-            height={500}
+            width={1200}
+            height={800}
+            style={{ width: '100%', height: 'auto' }}
             className="rounded-xl"
           />
         </div>
