a6cbea5a569d3c574f2b54794f39877e074ec7f7
Diego Massarni <webdiegomassarini@gmail.com>
2025-07-27 16:14:52 +0200

    fix:contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,8 +2,8 @@ import { AtSymbolIcon, PhoneIcon } from '@heroicons/react/24/solid'
 
 export default function Contact() {
   return (
-    <div className="bg-white mt-20 px-6">
-      <div className="mx-auto max-w-2xl lg:max-w-7xl px-6 lg:px-8 bg-slate-200/50 py-16  rounded-xl">
+    <div className="bg-white pyt-10 px-6">
+      <div className="mx-auto max-w-2xl lg:max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
           <div
             className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3"
