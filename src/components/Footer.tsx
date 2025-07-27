366883d9919aca365a01c0603f5a428be9a97afa
Diego Massarni <webdiegomassarini@gmail.com>
2025-07-27 16:15:33 +0200

    add link to instagram

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@ const navigation = [
   },
   {
     name: 'Instagram',
-    href: '#',
+    href: 'https://www.instagram.com/alberobello_ristorante/',
     icon: (props: any) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
